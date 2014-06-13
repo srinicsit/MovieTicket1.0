734f885c16ce5fe4bf5cc07d67a86df2f3d1d72d
srinicsit <srini.csit@gmail.com>
2014-06-13 22:55:39 +0530

    updated

--- MovieTicket/MovieTicket-bs/src/main/java/com/avihs/movie/business/theatermgmt/service/TheaterMgmtServiceImpl.java
+++ MovieTicket/MovieTicket-bs/src/main/java/com/avihs/movie/business/theatermgmt/service/TheaterMgmtServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import com.avihs.movie.business.theatermgmt.dao.TheaterMgmtDao;
 
 @Service
-public class TheaterMgmtServiceImpl {
+public class TheaterMgmtServiceImpl implements TheaterMgmtService {
 
 	public TheaterMgmtServiceImpl() {
 		System.out.println("TheaterMgmtServiceImpl");
